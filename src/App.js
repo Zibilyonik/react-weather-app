4dd21f9814aa42d56026dbb3394410dbdea7d178
Zibilyonik <onatsurmeli@gmail.com>
2024-05-24 13:44:47 +0200

    Import FetchData function into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import FetchData from './api';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FetchData />
         <a
           className="App-link"
           href="https://reactjs.org"
