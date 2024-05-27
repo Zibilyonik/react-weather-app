ddaa4d5b8469f06b0bc66816dc686c1fcc0ba9a3
Onur <onatsurmeli@gmail.com>
2024-05-27 18:19:06 +0200

    Remove returned component from api.jsx and switch component to hook

--- src/api.jsx
+++ src/api.jsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-const FetchData = ({ latitude, longitude, forecastDays, hourlyParams }) => {
-  const [retrieved, setRetrieved] = useState(false);
-  const [data, setData] = useState(null);
+const useFetchData = ({ latitude, longitude, forecastDays, hourlyParams }) => {
+  const [data, setData] = useState("waiting for data...");
   const [error, setError] = useState(false);
   const API_URL = `https://api.open-meteo.com/v1/gem?latitude=${latitude}&longitude=${longitude}&hourly=${hourlyParams.join(
     ","
   )}&forecast_days=${forecastDays}`;
   const sendRequest = async () => {
-    setRetrieved(false);
+    setData("waiting for data...");
     try {
       const response = await axios.get(API_URL);
       setData(response.data);
-      setRetrieved(true);
     } catch (error) {
       console.error("Error fetching data: ", error);
-      setRetrieved(false);
       setError(true);
     }
   };
@@ -26,12 +23,7 @@ const FetchData = ({ latitude, longitude, forecastDays, hourlyParams }) => {
     const timer = setInterval(sendRequest, 10000);
     return () => clearInterval(timer);
   }, [latitude, longitude, forecastDays, hourlyParams]);
-  const output = retrieved ? (
-    <div>Data retrieved: {JSON.stringify(data)}</div>
-  ) : (
-    <div>Fetching data...</div>
-  );
-  return error ? <div>Error fetching data</div> : output;
+  return data;
 };
 
-export default FetchData;
+export default useFetchData;
