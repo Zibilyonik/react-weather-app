70d023f630d3dd118d72cc66f1834119f1a7a7ab
Onur <onatsurmeli@gmail.com>
2024-06-04 17:49:27 +0200

    Implement Router into project root

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import React from 'react';
-import Display from './Display';
+import "./App.css";
+import React from "react";
+import Display from "./Display";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <Display />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/:city?/:options?" element={<Display />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
